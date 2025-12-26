3358caea14d3a0c64da44dd72eb8ccb525974c6a
vk datta <134008395+vkdatta@users.noreply.github.com>
2025-12-26 14:21:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "chartink=chartink:main"
+            "chartink=chartinkbyvk:main"
         ]
     },
     python_requires=">=3.8"
