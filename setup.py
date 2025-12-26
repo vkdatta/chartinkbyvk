489efcbee006a3376f4f4db445baff9400ee1baa
vk datta <134008395+vkdatta@users.noreply.github.com>
2025-12-26 15:00:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="chartinkbyvk",
     version="0.0",
-    py_modules=["chartink"],
+    py_modules=["chartinkbyink"],
     install_requires=[
         "requests",
         "pandas",
